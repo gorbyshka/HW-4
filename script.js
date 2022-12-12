90e5ec3591c5327cd315c02705d98d3a48161e13
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2022-12-12 14:53:59 +0200

    first commit

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const firstNumber = Number(prompt('Введіть перше число'));
-const secondNumber = Number(prompt('Введіть друге число'));
-
-const questAdd = confirm('+');
-const questSub = confirm('-');
-const questMult = confirm('*');
-const questDiv = confirm('/');
+const firstNumber = Number(prompt('Введіть перше число?'));
+const secondNumber = Number(prompt('Введіть друге число?'));
+const operation = prompt('Введіть операцію');
 
 const addResult = firstNumber + secondNumber;
 const subResult = firstNumber - secondNumber;
 const multResult = firstNumber * secondNumber;
 const divResult = firstNumber / secondNumber;
 
-if (questAdd == ! false) {
-    alert('Користувач ввів ' + ': ' + firstNumber + '+' + secondNumber + '=' + addResult);
-} else if (questSub == ! false) {
-    alert('Користувач ввів ' + ': ' + firstNumber + '-' + secondNumber + '=' + subResult);
-} else if (questMult == ! false) {
-    alert('Користувач ввів ' + ': ' + firstNumber + '*' + secondNumber + '=' + multResult);
-} else if (questDiv == ! false) {
-    alert('Користувач ввів ' + ': ' + firstNumber + '/' + secondNumber + '=' + divResult);
-} else {
-    alert('Тоді рахуйте самі:)');
-}
+switch (operation) {
+    case '+':
+        alert('Користувач ввів ' + ': ' + firstNumber + '+' + secondNumber + '=' + addResult);
+        break;
+    case '-':
+        alert('Користувач ввів ' + ': ' + firstNumber + '-' + secondNumber + '=' + subResult);
+        break;
+    case '*':
+        alert('Користувач ввів ' + ': ' + firstNumber + '*' + secondNumber + '=' + multResult);
+        break;
+    case '/':
+        alert('Користувач ввів ' + ': ' + firstNumber + '/' + secondNumber + '=' + divResult);
+        default:
+            alert('Ви не ввели операцію');
+};
